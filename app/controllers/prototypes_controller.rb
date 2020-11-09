55b829a5b66d84486bb8ea096216a4eea05d27cd
Reo-Miyagawa <atethxhrimatggg@gmail.com>
2020-11-09 17:49:35 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,58 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: :edit
+
+  def index
+    @prototypes = Prototype.all
+  end
+  
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.create(prototype_params)
+    if @prototype.save
+      redirect_to root_path  
+    else
+      render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    if current_user.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    render :index
+  end
+
+  private
+
+  def prototype_params
+    params.permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+  # 何故不要か＝require(:prototype)
+
+  def move_to_index
+    unless current_user == @prototype
+      redirect_to action: :index
+    end
+  end
+end
